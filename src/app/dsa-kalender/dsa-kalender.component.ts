4e04b4497b146356559633d1a737d03c28d0f7f4
Failender <patrick.hilgenstock@haw-hamburg.de>
2018-06-21 20:24:45 +0200

    test "fixes"

--- src/app/dsa-kalender/dsa-kalender.component.ts
+++ src/app/dsa-kalender/dsa-kalender.component.ts
@@ -21,16 +21,6 @@ export class DsaKalenderComponent implements OnInit {
   ngOnInit() {
     this.kalenderService.buildMonth(this.heute, this.gruppe)
       .subscribe(data => this.monat = data);
-    // this.monat.tage[4].events = [
-    //   {
-    //     color: 'red',
-    //     name: 'THIS IS AN EVENT YES HELLO'
-    //   },
-    //   {
-    //     color: '',
-    //     name: 'Baguette'
-    //   }
-    ];
   }
 
   naechsterMonat() {
