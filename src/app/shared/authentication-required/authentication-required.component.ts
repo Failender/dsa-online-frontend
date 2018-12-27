24d69ea301a4eba0170b3714d064a1ce293b869f
Failender <patrick.hilgenstock@haw-hamburg.de>
2018-12-27 16:27:36 +0100

    Fix authentitaction required component

--- src/app/shared/authentication-required/authentication-required.component.ts
+++ src/app/shared/authentication-required/authentication-required.component.ts
@@ -1,6 +1,7 @@
 import {OnInit} from '@angular/core';
 import {AuthenticationService} from '../service/authentication/authentication.service';
 import {RoutingService} from "../routing.service";
+import {first} from 'rxjs/operators';
 
 export abstract class AuthenticationRequiredComponent implements OnInit {
 
@@ -8,18 +9,23 @@ export abstract class AuthenticationRequiredComponent implements OnInit {
   }
 
   ngOnInit() {
-    if  (this.authenticationService.authenticated) {
-      if (this.neededRight()) {
-        if (!this.authenticationService.rights.includes(this.neededRight())) {
-          console.debug('User does not got the permission ' + this.neededRight() + ' routing')
+    this.authenticationService.initialized
+      .pipe(first())
+      .subscribe(() => {
+        if  (this.authenticationService.authenticated) {
+          if (this.neededRight()) {
+            if (!this.authenticationService.rights.includes(this.neededRight())) {
+              console.debug('User does not got the permission ' + this.neededRight() + ' routing')
+              this.router.navigateByUrl('/home');
+            }
+          }
+          this.init();
+        } else {
           this.router.navigateByUrl('/home');
+          console.debug('User is not logged in - routing')
         }
-      }
-      this.init();
-    } else {
-      this.router.navigateByUrl('/home');
-      console.debug('User is not logged in - routing')
-    }
+      })
+
   }
 
   protected neededRight(): string {
